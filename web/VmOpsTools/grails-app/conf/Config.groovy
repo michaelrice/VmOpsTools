e1fa7b65f3764712563dc1ed9b27ad5ae51c8b05
Michael Rice <michael@michaelrice.org>
2013-09-24 07:29:34 +0000

    added place holder for vcenter service user

--- web/VmOpsTools/grails-app/conf/Config.groovy
+++ web/VmOpsTools/grails-app/conf/Config.groovy
@@ -118,3 +118,8 @@ log4j = {
 grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.toastcoders.VmOpsTools.User'
 grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.toastcoders.VmOpsTools.UserRole'
 grails.plugins.springsecurity.authority.className = 'com.toastcoders.VmOpsTools.Role'
+
+vcenter {
+    admin_user = "myuser"
+    admin_pass = "secure_password"
+}
